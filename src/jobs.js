937b3f61293954ad0b88425b9fd373ea2e017aee
turtle89431 <turtle89431@gmail.com>
2018-02-22 17:03:50 -0800

    more adjustments

--- src/jobs.js
+++ src/jobs.js
@@ -25,7 +25,7 @@ let Jtable=props=>{
         </tbody>
     </table>
     </div>
-    <div className="col-4">
+    <div className="col-3">
     <h3 className="col-12">National industry trends</h3>
     <BLS data={d ||{}}/>
     </div>
