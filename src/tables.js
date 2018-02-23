3989a5cb03c76155281939195832d01b598e526f
turtle89431 <turtle89431@gmail.com>
2018-02-22 20:15:37 -0800

    745px

--- src/tables.js
+++ src/tables.js
@@ -16,7 +16,7 @@ let Tables =props=>{
         </div>
     </div>
     <div className="row">
-        <div className="col-6">
+        <div className="col-6 jc">
         <h3>Butte County</h3>
         <Butte header={this.props.butte.headers ||{}} data ={this.props.butte.data || {}} /></div>
         <div className="col-6">
@@ -24,16 +24,16 @@ let Tables =props=>{
         <Butte header={this.props.shasta.headers ||{}} data ={this.props.shasta.data || {}} /></div>
     </div>
     <div className="row">
-    <div className="col-6">
+    <div className="col-6 jc">
         <h3>Del Norte, Humboldt, Lake &amp; Mendocino Counties</h3>
         <Butte header={this.props.nc.headers ||{}} data ={this.props.nc.data || {}} /></div>
         
-        <div className="col-6">
+        <div className="col-6 jc">
         <h3>Lassen, Modoc, Nevada, Plumas, Sierra, Siskiyou &amp; Trinity Counties</h3>
         <Butte header={this.props.plumas.headers ||{}} data ={this.props.plumas.data ||{}} /></div>
     </div>
     <div className="row">
-    <div className="col-6">
+    <div className="col-6 jc">
         <h3>Colusa, Glenn, &amp; Tehama Counties</h3>
         <Butte header={this.props.glen.headers ||{}} data ={this.props.glen.data || {}} /></div>
         <div className="col-6">
